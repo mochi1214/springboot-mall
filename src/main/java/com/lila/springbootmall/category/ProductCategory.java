f83599d519c692db0fd9dc195464cad9fbbb970c
ChiaChen <tn00817411@gmail.com>
2024-11-18 19:50:24 +0800

    商品功能實作

---
+++ src/main/java/com/lila/springbootmall/category/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.lila.springbootmall.category;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
---
+++ src/main/java/com/lila/springbootmall/controller/ProductController.java
@@ -0,0 +1,111 @@
+package com.lila.springbootmall.controller;
+
+import com.lila.springbootmall.category.ProductCategory;
+import com.lila.springbootmall.dto.ProductQueryParams;
+import com.lila.springbootmall.dto.ProductRequest;
+import com.lila.springbootmall.model.Product;
+import com.lila.springbootmall.service.ProductService;
+import com.lila.springbootmall.util.Page;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // 查詢所有商品
+    @GetMapping("/products")
+    public ResponseEntity<Page<Product>> getAllProducts(
+            // 查詢條件 Filtering
+            @RequestParam(required = false) ProductCategory category,
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // 分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
+            ){
+
+        ProductQueryParams productQueryParams = new ProductQueryParams();
+        productQueryParams.setCategory(category);
+        productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
+
+        // 取得 Product list
+        List<Product> productList = productService.getAllProducts(productQueryParams);
+
+        // 取得 Product 總筆數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
+    }
+
+    // 查詢商品
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer productId) {
+        Product product = productService.getProductById(productId);
+
+        if (product != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductRequest productRequest) {
+        Integer productId = productService.createProduct(productRequest);
+
+        Product product = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        productService.updateProduct(productId, productRequest);
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
