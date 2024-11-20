92f7887fea82b847c2a5fde08c1d2a6ee4c49e90
ChiaChen <tn00817411@gmail.com>
2024-11-20 19:06:16 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/lila/springbootmall/service/ProductService.java
+++ src/main/java/com/lila/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.lila.springbootmall.service;
 
+import com.lila.springbootmall.dto.ProductRequest;
 import com.lila.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/lila/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/lila/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lila.springbootmall.service.impl;
 
 import com.lila.springbootmall.dao.ProductDao;
+import com.lila.springbootmall.dto.ProductRequest;
 import com.lila.springbootmall.model.Product;
 import com.lila.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getProductById(Integer productId) {
         return productDao.getProductById(productId);
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+        return productDao.createProduct(productRequest);
     }
 }
