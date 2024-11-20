df380ecdd4f18dd76a3496265f29319d9f86f799
ChiaChen <tn00817411@gmail.com>
2024-11-20 19:06:16 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/lila/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/lila/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,16 +1,23 @@
 package com.lila.springbootmall.dao.impl;
 
 import com.lila.springbootmall.dao.ProductDao;
+import com.lila.springbootmall.dto.ProductRequest;
 import com.lila.springbootmall.model.Product;
 import com.lila.springbootmall.rowapper.ProductRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
+
 
 @Component
 public class ProductDaoImpl implements ProductDao {
@@ -34,5 +41,33 @@ public class ProductDaoImpl implements ProductDao {
         } else {
             return null;
         }
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+        String sql = "INSERT INTO product (product_name, category, image_url, price, stock," +
+                " description, created_date, last_modified_date) " +
+                "VALUES (:productName, :category, :imageUrl, :price, :stock, :description, " +
+                ":created_date, :last_modified_date);";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+
+        Date now = new Date();
+        map.put("created_date", now);
+        map.put("last_modified_date", now);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map), keyHolder);
+
+        int productId = keyHolder.getKey().intValue();
+
+        return productId;
     }
 }
