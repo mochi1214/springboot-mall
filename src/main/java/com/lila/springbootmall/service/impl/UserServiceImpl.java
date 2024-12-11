72b117e157041b3160b946e938c0ecc3549aa151
ChiaChen <tn00817411@gmail.com>
2024-12-11 14:59:41 +0800

    帳號功能 - 登入

--- src/main/java/com/lila/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/lila/springbootmall/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lila.springbootmall.service.impl;
 
 import com.lila.springbootmall.dao.UserDao;
+import com.lila.springbootmall.dto.UserLoginRequest;
 import com.lila.springbootmall.dto.UserRegisterRequest;
 import com.lila.springbootmall.model.User;
 import com.lila.springbootmall.service.UserService;
@@ -36,5 +37,23 @@ public class UserServiceImpl implements UserService {
 
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
+    }
+
+    @Override
+    public User login(UserLoginRequest userLoginRequest) {
+
+        User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        if (user == null) {
+            log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+            return user;
+        } else {
+            log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
