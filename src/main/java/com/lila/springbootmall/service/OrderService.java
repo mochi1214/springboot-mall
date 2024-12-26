17b36adbd43649437ee4b5eefc080ed6a3bb68fb
ChiaChen <tn00817411@gmail.com>
2024-12-26 17:43:53 +0800

    訂單功能 - 完整訂單資訊

--- src/main/java/com/lila/springbootmall/service/OrderService.java
+++ src/main/java/com/lila/springbootmall/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.lila.springbootmall.service;
 
 import com.lila.springbootmall.dto.CreateOrderRequest;
+import com.lila.springbootmall.model.Order;
 
 public interface OrderService {
+
+    Order getOrderById(Integer orderId);
 
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
