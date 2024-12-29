ec953f55b4d0480edc78975c2b29948877936de0
ChiaChen <tn00817411@gmail.com>
2024-12-30 02:27:04 +0800

    訂單功能 - 查詢訂單列表

--- src/main/java/com/lila/springbootmall/dao/OrderDao.java
+++ src/main/java/com/lila/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.lila.springbootmall.dao;
 
+import com.lila.springbootmall.dto.OrderQueryParams;
 import com.lila.springbootmall.model.Order;
 import com.lila.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
--- src/main/java/com/lila/springbootmall/service/OrderService.java
+++ src/main/java/com/lila/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.lila.springbootmall.service;
 
 import com.lila.springbootmall.dto.CreateOrderRequest;
+import com.lila.springbootmall.dto.OrderQueryParams;
 import com.lila.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
