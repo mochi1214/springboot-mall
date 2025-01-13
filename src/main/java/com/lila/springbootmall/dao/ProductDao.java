f796434da2d4ff58de0cc5604bc4ec9c2f337792
ChiaChen <tn00817411@gmail.com>
2025-01-13 18:24:42 +0800

    Revert "商品功能 - 實作查詢商品"
    This reverts commit 553a8eb5c886ed4923d073f6b8c2bced730708f0.

--- src/main/java/com/lila/springbootmall/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.lila.springbootmall.dao;
-
-import com.lila.springbootmall.model.Product;
-
-public interface ProductDao {
-
-    Product getProductById(Integer productId);
-}
--- src/main/java/com/lila/springbootmall/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.lila.springbootmall.service;
-
-import com.lila.springbootmall.model.Product;
-
-public interface ProductService {
-
-    Product getProductById(Integer productId);
-}
